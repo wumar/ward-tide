df305d8b392296a15b763d1bddc2e3a7ebd5e513
wumar <25637612+wumar@users.noreply.github.com>
2022-04-13 10:07:30 +0200

    manually fix DNS

--- wrapper.sh
+++ wrapper.sh
@@ -3,5 +3,6 @@ set -m
 ./run_setup.sh > pia_log.txt &
 sleep 30
 echo "Connection\PortRangeMin=$(tail pia_log.txt | grep Forwarded | grep -o '.....$')" >> /root/.config/qBittorrent/qBittorrent.conf
+echo "nameserver 10.0.0.243" >> /etc/resolv.conf
 tinyproxy &
 qbittorrent-nox
